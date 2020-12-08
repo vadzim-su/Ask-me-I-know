454be060cf747eb80deaeb14a8fb39b8369dc097
Vadzim Suschynski <vadzim.sushchynski@gmail.com>
2020-12-08 16:38:54 +0300

    feat: add validation of forms

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -3,13 +3,25 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
-  styleUrls: ['./login.component.scss']
+  styleUrls: ['./login.component.scss'],
 })
 export class LoginComponent implements OnInit {
+  warningText: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  getInfo($event, email, password): void {
+    $event.preventDefault();
+    if (email) {
+      if (password) {
+        location.href = '/home';
+      } else {
+        this.warningText = 'Enter your password';
+      }
+    } else {
+      this.warningText = 'Enter your email';
+    }
   }
-
 }
--- src/app/registration/registration.component.ts
+++ src/app/registration/registration.component.ts
@@ -3,13 +3,40 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-registration',
   templateUrl: './registration.component.html',
-  styleUrls: ['./registration.component.scss']
+  styleUrls: ['./registration.component.scss'],
 })
 export class RegistrationComponent implements OnInit {
+  warningText: string;
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  isValidEnteredData(
+    $event,
+    username,
+    email,
+    password,
+    confirmedPassword
+  ): void {
+    $event.preventDefault();
+
+    if (/^[a-zA-Z0-9_-]{3,}/.test(username)) {
+      if (/^[a-zA-Z0-9._-]+@[a-z]+\.[a-z]{2,3}$/.test(email)) {
+        if (/^[a-zA-Z0-9]{5,}/.test(password)) {
+          if (password === confirmedPassword) {
+            location.href = '/home';
+          } else {
+            this.warningText = 'Entered passwords are not the same';
+          }
+        } else {
+          this.warningText = 'Your password is not valid';
+        }
+      } else {
+        this.warningText = 'Your email is not valid';
+      }
+    } else {
+      this.warningText = 'Your username is not valid';
+    }
   }
-
 }
