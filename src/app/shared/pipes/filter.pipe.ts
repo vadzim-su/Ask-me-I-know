14b882b7cc4ac99215c45aaa486e7338ffd564fb
Vadzim Suschynski <vadzim.sushchynski@gmail.com>
2021-01-26 16:30:23 +0300

    feat: fix pipe using functions

--- src/app/shared/pipes/filter.pipe.ts
+++ src/app/shared/pipes/filter.pipe.ts
@@ -3,6 +3,7 @@ import Question from '../interfaces/question.model';
 import { AuthService } from '../services/auth.service';
 import { FilterService } from '../services/filter.service';
 import { tags } from '../data/tags';
+import Filter from '../interfaces/filter.model';
 
 @Pipe({
   name: 'filter',
@@ -13,57 +14,68 @@ export class FilterPipe implements PipeTransform {
     private filterService: FilterService,
     private authService: AuthService
   ) {}
-  transform(questions: Question[], params: any): Question[] {
-    const now = new Date();
+  transform(questions: Question[]): Question[] {
     return questions?.filter(
       (question) =>
-        (this.filterService.filters.questions === 'all'
-          ? question
-          : this.filterService.filters.questions === 'answered'
-          ? question.hasSolution
-          : !question.hasSolution) &&
-        (this.filterService.filters.moderation === 'all'
-          ? question
-          : this.filterService.filters.moderation === 'approved'
-          ? question.isModerated
-          : !question.isModerated) &&
-        (this.filterService.filters.owner === 'all'
-          ? question
-          : this.filterService.filters.owner === 'my questions'
-          ? question.author === this.authService.userEmail
-          : question.author !== this.authService.userEmail) &&
-        (this.filterService.filters.periods === 'all'
-          ? question
-          : this.filterService.filters.periods === 'day'
-          ? +now - question.date <= 1000 * 60 * 60 * 24
-          : this.filterService.filters.periods === 'week'
-          ? +now - question.date <= 1000 * 60 * 60 * 24 * 7
-          : +now - question.date <= 1000 * 60 * 60 * 24 * 30)
-      // &&
-      // (!this.filterService.filters.categories.length ||
-      // this.findSamesCategories(question, this.filterService.filters)
-      //   ? question
-      //   : null)
+        this.findAnsweredQuestions(question, this.filterService.filters) &&
+        this.findModeratedQuestions(question, this.filterService.filters) &&
+        this.findOwnerQuestions(question, this.filterService.filters) &&
+        this.findPeriodsQuestions(question, this.filterService.filters) &&
+        (!this.filterService.filters.categories.length ||
+          this.findSamesCategories(question, this.filterService.filters))
     );
   }
 
-  // findAnsweredQuestions(question: Question) {
-  //   this.filterService.filters.questions === 'all'
-  //     ? question
-  //     : this.filterService.filters.questions === 'answered'
-  //     ? question.hasSolution
-  //     : !question.hasSolution;
-  // }
+  findAnsweredQuestions(question: Question, filter: Filter) {
+    let output =
+      filter.questions === 'all'
+        ? question
+        : filter.questions === 'answered'
+        ? question.hasSolution
+        : !question.hasSolution;
+    return output;
+  }
 
-  findSamesCategories(question, filter) {
+  findModeratedQuestions(question: Question, filter: Filter) {
+    let output =
+      filter.moderation === 'all'
+        ? question
+        : filter.moderation === 'approved'
+        ? question.isModerated
+        : !question.isModerated;
+    return output;
+  }
+
+  findOwnerQuestions(question: Question, filter: Filter) {
+    let output =
+      filter.owner === 'all'
+        ? question
+        : filter.owner === 'my questions'
+        ? question.author === this.authService.userEmail
+        : question.author !== this.authService.userEmail;
+    return output;
+  }
+
+  findPeriodsQuestions(question: Question, filter: Filter) {
+    const now = new Date();
+    let output =
+      filter.periods === 'all'
+        ? question
+        : filter.periods === 'day'
+        ? +now - question.date <= 1000 * 60 * 60 * 24
+        : filter.periods === 'last week'
+        ? +now - question.date <= 1000 * 60 * 60 * 24 * 7
+        : +now - question.date <= 1000 * 60 * 60 * 24 * 30;
+    return output;
+  }
+
+  findSamesCategories(question: Question, filter: Filter) {
+    let isOutput = false;
     filter.categories.forEach((cat) => {
       if (question.tags.includes(cat)) {
-        console.log(true);
-        return true;
-      } else {
-        // console.log(false);
-        // return false;
+        isOutput = true;
       }
     });
+    return isOutput ? question : null;
   }
 }
